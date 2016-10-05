c4be5c337e59bc587c44f380ea7593a580f5a267
smu2013anwanjin <smu2013anwanjin@users.noreply.github.com>
2016-10-06 02:12:34 +0900

    Add files via upload

---
+++ class Player.java
@@ -0,0 +1,37 @@
+ class Dice {
+   private int number = 0;
+   public void roll() {
+     number = (int) (Math.random() * 6);
+     System.out.println("turn on" + number);
+   }
+   public int getNum() {
+     return this.number;
+   }
+ }
+
+\기존 class Dice에서 public int getNum()를 추가하여 Player와 연동
+
+ class Player {
+   private String name=null;
+   private int num=0;
+   public Player(String _name) {
+     name= _name;
+   }
+   public void play() {
+     Dice a=new Dice();
+     a.roll();
+     num=a.getNum();
+   }
+   public String getName() {
+     return this.name;
+   }
+   public int getNum() {
+     return this.num;
+   }
+ }
+
+
+ Player w= new Player("wanjin");
+
+
+ w.play();
