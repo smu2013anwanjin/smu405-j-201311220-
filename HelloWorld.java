cc53fca22648bc759ef024bb605daa8cc6f22410
smu2013anwanjin <smu2013anwanjin@users.noreply.github.com>
2016-10-15 20:20:18 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sd.hello;
+
+public class HelloWorld {
+  public void hello() { 
+     System.out.println("Hello World from hello()");
+  }
+  public void hello(String name) {
+     System.out.printf("Hello World is %s", name);
+  }
+  public static void main(String[] args) {
+     System.out.println("Hello World");
+  }
+}
