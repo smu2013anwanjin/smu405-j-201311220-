f51b842aaaf9aa82f19ae957431baf29cbe7c66f
smu2013anwanjin <smu2013anwanjin@users.noreply.github.com>
2016-10-15 20:20:55 +0900

    Add files via upload

---
+++ TulteDemoFile.java
@@ -0,0 +1,16 @@
+
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+class TurtleDemoMain{
+	TurtleDemoMain(){
+		Turtle t1=new Turtle();
+		for(int i=0;i<4;i++){
+			t1.forward(50);
+			t1.left(90);
+		}
+	}
+	public static void main(String[] args){
+		new TurtleDemoMain();
+	}
+}
